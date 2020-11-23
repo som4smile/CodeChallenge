bee264dfa813c3b163af3d40900b5acedaa12cee
SOM <som4smile@gmail.com>
2020-11-24 02:41:54 +0530

    Initial UI Setup and API Integration Done.

---
+++ CodeChallenge/CodeChallenge/Data Model/ContentData.swift
@@ -0,0 +1,32 @@
+//
+//  ContentData.swift
+//  CodeChallenge
+//
+//  Created by SOM on 24/11/20.
+//  Copyright © 2020 Somnath Rasal. All rights reserved.
+//
+
+import Foundation
+
+struct ContentData: Codable {
+    var id: String?
+    var type: String?
+    var date: String?
+    var data: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, type, date, data
+    }
+}
+
+extension ContentData {
+    var dateConverted: Date {
+        guard let date = self.date else { return Date() }
+        return dateFormatter.date(from: date) ?? Date()
+    }
+    private var dateFormatter: DateFormatter {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }
+}
