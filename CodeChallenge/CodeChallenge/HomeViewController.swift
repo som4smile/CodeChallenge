bf6b00f957eaa957aaa4fbc7fb77e4c2ed430fd7
SOM <som4smile@gmail.com>
2020-11-23 23:01:17 +0530

    Basic and Pod setup

---
+++ CodeChallenge/CodeChallenge/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  CodeChallenge
+//
+//  Created by SOM on 23/11/20.
+//  Copyright © 2020 Somnath Rasal. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
